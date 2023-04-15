0b3a0115c40a3efe678ede4a7b82d2783bfb682d
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2023-04-14 22:37:12 -0500

    Convert number to reversed array of digits

---
+++ 8kyu/reverseDigitizeArr.js
@@ -0,0 +1,13 @@
+/* 
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example(Input => Output):
+35231 => [1,3,2,5,3]
+0 => [0]
+*/
+function digitize(numbers) {
+    //code here
+    return numbers.toString().split('').reverse().map(num => parseInt(num) )
+    
+}
